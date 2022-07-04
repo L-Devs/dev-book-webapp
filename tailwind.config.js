fc783545eecfb31a29caecf06a75d6156d82dc9e
mosalah <almoaz065@gmail.com>
2022-07-04 15:58:43 +0200

    chore: added color palette and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"accent": {
+					100: "#8A77FF",
+					200: "#654DFF",
+					300: "#4F33FF"
+				},
+
+				"dark": {
+					100: "#302F32",
+					200: "#242424",
+					300: "#121212"
+				},
+
+				"light": {
+					100: "#FAFAFA",
+					200: "#E4E5F0",
+					300: "#D0D1DF"
+				}
+			},
+			fontFamily: {
+				'poppins': ['Poppins', 'sans-serif'] 
+			}
+		},
 	},
 	plugins: [],
 }
