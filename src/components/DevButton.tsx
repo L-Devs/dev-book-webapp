35bf9c44e860f7e76c8e00b767d60a017ec92e40
mosalah <almoaz065@gmail.com>
2022-07-05 09:11:53 +0200

    added button component

--- src/components/DevButton.tsx
+++ src/components/DevButton.tsx
@@ -1,6 +1,28 @@
-const DevButton: React.FC = () => {
-	return <></>;
+import React, { FunctionComponent } from "react";
+
+interface Props extends React.HTMLProps<HTMLButtonElement> {
+	text: string;
+	type: "accent" | "secondary" | "text-accent" | "text-secondary";
+	cb: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const DevButton:FunctionComponent<Props> = ({ type, text, cb}) => {
+	return (
+		<button
+			onClick={(e) => cb(e)}
+			className={`text-sm md:text-base font-bold capitalize text-center px-5 py-[10px] md:px-6 md:py-3  lg:text-lg lg:px-7 lg:py-[14px] rounded-lg ${
+				type == "accent"
+					? "bg-accent-200 hover:bg-accent-100 text-white"
+					: type == "secondary"
+						? "bg-dark-200 hover:bg-dark-100 text-white dark:bg-light-100 dark:hover:bg-light-200 dark:text-dark-200"
+						: type == "text-accent"
+							? "text-accent-200"
+							: type == "text-secondary"
+								? "text-dark-200 dark:text-light-200"
+								: " "}`}>
+			{text}
+		</button>
+	);
 };
 
 export default DevButton;
-
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,9 +1,22 @@
+import React from "react";
+import DevButton from "../components/DevButton";
 import DevInput from "../components/DevInput";
 
 const SignUp: React.FC = () => {
-	return <>
-		<DevInput />
-	</>;
+	const handleButtonClick = (evnet: React.MouseEvent<HTMLButtonElement>) => {
+		console.log("button clicked");
+	}
+	return (
+		<>
+			<DevInput />
+			<div className="flex gap-4 p-4 flex-wrap">
+				<DevButton text="create account" type="accent" cb={handleButtonClick}/>
+				<DevButton text="create account" type="secondary" cb={handleButtonClick}/>
+				<DevButton text="create account" type="text-accent" cb={handleButtonClick}/>
+				<DevButton text="create account" type="text-secondary" cb={handleButtonClick}/>
+			</div>
+		</>
+	);
 };
 
 export default SignUp;
