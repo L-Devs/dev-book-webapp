6c1e59df51fb098de4052843c76ee9e7d5a0ba2c
Omar Kamal <OmarQurashi868@gmail.com>
2022-07-05 21:59:07 +0400

    Made banner into a component

---
+++ src/components/PageBanner.tsx
@@ -0,0 +1,31 @@
+interface Props {
+	alternate?: boolean;
+}
+// Use alternate prop to invert background and text colors depending on the wanted theme
+
+const PageBanner = ({ alternate }: Props) => {
+	let bgColor = "bg-accent-200";
+	let textColor = "text-light-100";
+
+	if (alternate) {
+		bgColor = "bg-light-100";
+		textColor = "text-accent-200";
+	}
+
+	return (
+		<div className={bgColor + " w-1/2 h-screen content-center"}>
+			<div className="flex justify-center h-screen items-center">
+				<h1 className={textColor + " w-2/3 text-7xl text-left leading-normal"}>
+					<span>
+						Share your Interests, <br></br>Stay
+					</span>
+					<span className="w-[477px] bg-dark-200 h-[155px] mt-3 text-center flex items-center justify-center -rotate-[5.2deg] text-light-100">
+						Connected
+					</span>
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default PageBanner;
