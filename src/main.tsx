3360224443d438dfcabbcd73338f51b374bb3350
Mustafa Ahmed <45480939+MidfieId@users.noreply.github.com>
2022-07-05 22:59:52 +0530

    stylized left size of page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./style/index.css";
 
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 const root = document.getElementById("root") as HTMLElement;
 
@@ -15,6 +16,7 @@ if (root) {
 				<Routes>
 					<Route path="/" element={<SignUp />}></Route>
 					<Route path="/signup" element={<SignUp />}></Route>
+					<Route path="/login" element={<Login />}></Route>
 				</Routes>
 			</BrowserRouter>
 		</React.StrictMode>
