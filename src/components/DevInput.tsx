3807f224d5df7f488ec96dd9123e4e4e46b12146
mosalah <almoaz065@gmail.com>
2022-07-07 18:03:09 +0200

    useForm hook is working now

--- src/components/DevInput.tsx
+++ src/components/DevInput.tsx
@@ -1,23 +1,36 @@
+import React from "react";
+import { FormState } from "react-hook-form";
+import { schema } from "../pages/SignUp";
 // this is the type of the components props ..
 // it will extend the input element props cuz
 // since we want to customize the input element
-interface Props extends React.HTMLProps<HTMLInputElement> {
+interface Props<T> extends React.HTMLProps<HTMLInputElement> {
 	title?: string;
+	register: T;
+	// formState: FormState<typeof schema>;
 }
 
 // extracting the className so I can use the existing class names
 // and add the passed by the user of the component
-const DevInput: React.FC<Props> = ({ className, title, ...props }) => {
+function DevInput<T>({
+	className,
+	title,
+	register,
+	...props
+}: Props<T>) {
+	// const { errors } = formState;
+	// console.log(errors);
 	return (
 		<>
 			{/* this is the label and won't be rendered unless label prop passed */}
 			{title && <label className="mb-1 block font-medium">{title}</label>}
 			<input
 				{...props}
+				{...register}
 				className={`bg-light-200 border-0 rounded-lg px-4 py-2 md:px-5 md:py-3  lg:px-6 lg:py-3 block ${className}`}
 			/>
 		</>
 	);
-};
+}
 
 export default DevInput;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,30 +3,79 @@ import DevInput from "../components/DevInput";
 import StayConnectedBanner from "../components/StayConnectedBanner";
 import Logo from "../components/Logo";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+export const schema = z
+	.object({
+		userName: z
+			.string({
+				required_error: "userName is required",
+			})
+			.min(4)
+			.max(20),
+		emailAddress: z
+			.string({
+				required_error: "email is required",
+			})
+			.email("Not a valid email"),
+		password: z
+			.string({
+				required_error: "password is required",
+			})
+			.min(8),
+		confirmPassword: z.string(),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords do not match",
+		path: ["passwordConfirmation"],
+	});
+
+export type Schema = z.infer<typeof schema>;
 
 const SignUp: React.FC = () => {
+	const {
+		register,
+		handleSubmit,
+		formState,
+	} = useForm<Schema>({
+		resolver: zodResolver(schema),
+	});
+	const onSubmit = (data: Schema) => {
+		console.log(data);
+	};
 	return (
 		<div className="p-5 md:p-0 flex flex-col bg-light-100 min-h-screen md:flex-row">
 			<Logo className="mb-10 md:hidden text-accent-200" />
 			<StayConnectedBanner type="accent" className="mb-20 md:mb-0 md:w-1/2" />
 			<div className="md:w-1/2 md:pl-20 text-dark-200">
 				<Logo className="my-2 lg:my-4 xl:my-8 mx-auto hidden md:block lg:text-accent-200" />
-				<form className="flex flex-col gap-1 pb-2">
-					<h2 className="font-bold mb-4 capitalize text-dark-200 text-xl">Sign up</h2>
+				<form
+					className="flex flex-col gap-1 pb-2"
+					onSubmit={handleSubmit(onSubmit)}
+				>
+					<h2 className="font-bold mb-4 capitalize text-dark-200 text-xl">
+						Sign up
+					</h2>
 					<DevInput
 						title="User Name"
+						register={register("userName")}
 						className="mb-3 w-full md:w-9/12 2xl:w-6/12 text-dark-200"
 					/>
 					<DevInput
 						title="Email Address"
+						register={register("emailAddress")}
 						className="mb-3 w-full md:w-9/12 2xl:w-6/12 text-dark-200"
 					/>
 					<DevInput
 						title="Password"
+						register={register("password")}
 						className="mb-3 w-full md:w-9/12 2xl:w-6/12 text-dark-200"
 					/>
 					<DevInput
 						title="Confirm Password"
+						register={register("confirmPassword")}
 						className="w-full md:w-9/12 2xl:w-6/12 text-dark-200"
 					/>
 					<DevButton
