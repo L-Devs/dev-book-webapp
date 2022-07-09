5bb6f1a060c35c29c07d17874dbb60f9ee328044
mosalah <almoaz065@gmail.com>
2022-07-09 21:15:03 +0200

    fixed conflect on 'type' prop added btnType instead

--- src/components/DevButton.tsx
+++ src/components/DevButton.tsx
@@ -1,17 +1,23 @@
 interface Props extends React.HTMLProps<HTMLButtonElement> {
-	type: "accent" | "secondary" | "text-accent" | "text-secondary";
+	btnTypes: "accent" | "secondary" | "text-accent" | "text-secondary";
+	type?: "button" | "submit" | "reset" | undefined;
 }
 
-const DevButton: React.FC<Props> = ({ className, type, ...props }) => {
+const DevButton: React.FC<Props> = ({
+	className,
+	btnTypes,
+	children,
+	...props
+}) => {
 	let typeStyle = "";
 
-	if (type == "accent")
+	if (btnTypes == "accent")
 		typeStyle = "bg-accent-200 hover:bg-accent-100 text-white";
-	else if (type == "secondary")
+	else if (btnTypes == "secondary")
 		typeStyle =
 			"bg-dark-200 hover:bg-dark-100 text-white dark:bg-light-100 dark:hover:bg-light-200 dark:text-dark-200";
-	else if (type == "text-accent") typeStyle = "text-accent-200";
-	else if (type == "text-secondary")
+	else if (btnTypes == "text-accent") typeStyle = "text-accent-200";
+	else if (btnTypes == "text-secondary")
 		typeStyle = "text-dark-200 dark:text-light-200";
 
 	return (
@@ -19,7 +25,7 @@ const DevButton: React.FC<Props> = ({ className, type, ...props }) => {
 			{...props}
 			className={`md:text-base capitalize text-center px-5 py-[10px] md:px-6 md:py-3  lg:text-lg lg:px-7 lg:py-[14px] rounded-lg ${typeStyle} ${className}`}
 		>
-			{props.children}
+			{children}
 		</button>
 	);
 };
